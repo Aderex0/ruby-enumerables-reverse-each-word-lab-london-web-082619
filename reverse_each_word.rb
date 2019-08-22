35420f50a102821b6591217995edfb15e95775e5
Andrius Jurgsaitis <andriusj6@hotmail.com>
2019-08-22 23:22:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,8 @@ def reverse_each_word(string)
   reversed_array = array.collect { |x| x.reverse }
   reversed_array.join(" ")
 end
+
+def reverse_each_word(string)
+  i = 0
+  j = 0
+  new_array = [" "]
